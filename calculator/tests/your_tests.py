62a2a37ad3ba2ccad52738e1df336145e61c4538
Felipe Teixeira <flt@stanford.edu>
2023-05-14 17:55:14 -0700

    Updates

--- calculator/tests/your_tests.py
+++ calculator/tests/your_tests.py
@@ -4,6 +4,13 @@ from calculator_adapter import run
 
 ### ADD AT LEAST TWO TESTS HERE!
 
+assert run("90 + 13").output == "103"
+
+assert run("50 - 69").output == "-19"
+
+assert run("7 * 16").output == "112"
+
+assert run("200 / 40").output == "5"
 
 ###
 
